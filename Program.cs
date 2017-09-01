c60120a4fb46ccf7539018e3b1cfee1d1d47ab4d
vivekPasupathi <vivekPasupathi@.github.com>
2017-09-01 13:00:59 +0530

    CompleteRefrence - Part 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CLRViaC_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // //All types deived from system.Object
+           //int r =0; // int is struct
+          
+           //Program p = new Program();
+         
+           ////r.GetTypeCode()
+
+            int? r;
+            r = 10;
+            
+        }
+    }
+}
